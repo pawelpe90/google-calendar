6a7e3f2ef225483f9383d8cbb61ac85f8e857fd9
Pawel Pruszynski <Pawel.Pruszynski@tomtom.com>
2021-10-15 10:00:18 +0200

    Calendar updates added

--- calendar_handler.py
+++ calendar_handler.py
@@ -6,16 +6,28 @@ import pickle
 
 
 class Calendar:
-    def __init__(self):
+    def __init__(self, my_calendar_id):
         if not os.path.exists('token.pkl'):
             get_credentials()
         self.credentials = pickle.load(open('token.pkl', 'rb'))
         self.service = build('calendar', 'v3', credentials=self.credentials)
+        self.my_calendar_id = my_calendar_id
 
     @staticmethod
     def printer(iterate):
+        max_length = 0
+
         for i in iterate['items']:
-            print(i['summary'], i['id'])
+            length = len(i['summary'])
+            if length > max_length:
+                max_length = length
+
+        for i in iterate['items']:
+            length = len(i['summary'])
+            length_delta = max_length - length
+            space = " " * length_delta
+
+            print(f"Summary: {i['summary']} {space}| id: {i['id']} | start_time: {i['start']['dateTime']}")
 
     def display_all_calendars(self):
         result_calendar = self.service.calendarList().list().execute()
@@ -23,9 +35,31 @@ class Calendar:
         return result_calendar
 
     def add_event(self, summary, location, description, start_time):
-
-        my_calendar = '57k1mpn9cft8sll6nlibspre7k@group.calendar.google.com'
         end_time = start_time + timedelta(minutes=90)
-
         event = new_calendar_event(summary, location, description, start_time, end_time)
-        self.service.events().insert(calendarId=my_calendar, body=event).execute()
+        self.service.events().insert(calendarId=self.my_calendar_id, body=event).execute()
+
+    def list_events(self):
+        events = self.service.events().list(calendarId=self.my_calendar_id).execute()
+        self.printer(events)
+        return events
+
+    def update_event_summary(self, event_id, update):
+        event = self.service.events().get(calendarId=self.my_calendar_id, eventId=event_id).execute()
+        event['summary'] = update
+        self.service.events().update(calendarId=self.my_calendar_id, eventId=event['id'], body=event).execute()
+
+    def update_event_description(self, event_id, update):
+        event = self.service.events().get(calendarId=self.my_calendar_id, eventId=event_id).execute()
+        event['description'] = update
+        self.service.events().update(calendarId=self.my_calendar_id, eventId=event['id'], body=event).execute()
+
+    def update_event_location(self, event_id, update):
+        event = self.service.events().get(calendarId=self.my_calendar_id, eventId=event_id).execute()
+        event['location'] = update
+        self.service.events().update(calendarId=self.my_calendar_id, eventId=event['id'], body=event).execute()
+
+    def update_event_start_time(self, event_id, update):
+        event = self.service.events().get(calendarId=self.my_calendar_id, eventId=event_id).execute()
+        event['start']['dateTime'] = update.strftime('%Y-%m-%dT%H:%M:%S')
+        self.service.events().update(calendarId=self.my_calendar_id, eventId=event['id'], body=event).execute()
--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from util.scrapper import league_scrapper
 from datetime import datetime
 
 
-def main():
+def populate_calendar():
 
     games = league_scrapper()
-    c1 = Calendar()
+    c1 = Calendar('57k1mpn9cft8sll6nlibspre7k@group.calendar.google.com')
 
     for game in games:
         info = game['info']
@@ -16,8 +16,41 @@ def main():
         hour, mins = int(time.split(":")[0]), int(time.split(":")[1])
 
         start_time = datetime(year, month, dom, hour, mins, 0)
+        update_date = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
 
-        c1.add_event(game['game'], "SP nr 205", f"Sektor: {sector}\nWynik: {game['sets']}\n{game['points']}", start_time)
+        c1.add_event(game['game'], "SP nr 205", f"Sektor: {sector}\nWynik: {game['sets']}\n{game['points']}\n\n"
+                                                f"Ostatnia aktualizacja: {update_date}", start_time)
 
 
-main()
+def update_calendar_events():
+
+    games = league_scrapper()
+    c1 = Calendar('57k1mpn9cft8sll6nlibspre7k@group.calendar.google.com')
+
+    events = c1.list_events()
+
+    for event in events['items']:
+        for game in games:
+            if event['summary'] == game['game']:
+                info = game['info']
+                date, sector = info.split(",")[0], info.split(",")[1][-1]
+                day, time = date.split(" ")[0], date.split(" ")[-1]
+                year, month, dom = int(day.split("-")[0]), int(day.split("-")[1]), int(day.split("-")[2])
+                hour, mins = int(time.split(":")[0]), int(time.split(":")[1])
+
+                start_time = datetime(year, month, dom, hour, mins, 0)
+                update_date = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
+
+                c1.update_event_description(event['id'], f"Sektor: {sector}\nWynik: {game['sets']}\n{game['points']}\n"
+                                                         f"\nOstatnia aktualizacja: {update_date}")
+
+                c1.update_event_start_time(event['id'], start_time)
+
+
+def list_events():
+    c = Calendar('57k1mpn9cft8sll6nlibspre7k@group.calendar.google.com')
+    c.list_events()
+
+
+# list_events()
+update_calendar_events()
